3ae1c6b9a3d3054b1783b3516db81efed09bcf92
anshu3769 <37638104+anshu3769@users.noreply.github.com>
2018-05-17 11:53:32 -0400

    Update RunAllmodels.sh

--- RunAllmodels.sh
+++ RunAllmodels.sh
@@ -1,6 +1,8 @@
+#Change file permissios to run the script
+chmod 755 RunAllmodels.sh
+
 # Run random forest model
 python3 /data/WorkData/firmEmbeddings/Models/StockPredictionUsingRandomForest/RunRandomForest.py
-
 
 #Run Neural Network model for stock prediction
 python3 /data/WorkData/firmEmbeddings/Models/StockPredictionUsingNeuralNetwork/NeuralNetworkRun_3layers.py
