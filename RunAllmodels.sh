25f994e5ac6932d86b51d265c5f6baead99398cf
Anshu Tomar <Anshu@Anshus-Air.fios-router.home>
2018-05-17 11:19:59 -0400

    Run all the models

---
+++ RunAllmodels.sh
@@ -0,0 +1,9 @@
+# Run random forest model
+python3 /data/WorkData/firmEmbeddings/Models/StockPredictionUsingRandomForest/RunRandomForest.py
+
+
+#Run Neural Network model for stock prediction
+python3 /data/WorkData/firmEmbeddings/Models/StockPredictionUsingNeuralNetwork/NeuralNetworkRun_3layers.py
+
+#Run the Neura Network for firm embeddings
+python3 /data/WorkData/firmEmbeddings/Models/FirmEmbeddingsModel.py
